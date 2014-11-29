40cf676cd1c1f959123729e520fa185fbb6de321
Derek Martinez <me@derek-martinez.us>
2014-11-28 22:49:08 -0500

    Automatics

--- NightDay.py
+++ NightDay.py
@@ -15,18 +15,16 @@ class change_dayCommand(sublime_plugin.ApplicationCommand):
 		s.set("color_scheme", app_settings.get("day"))
 		sublime.save_settings("../User/Preferences.sublime-settings")
 
-'''
 def doTimeCheck():
 	hour = time.strftime("%H")
 	s = sublime.load_settings("../User/Preferences.sublime-settings")
 	if int(hour) >= 20:
-		if s.get("color_scheme") != app_settings.get("night"):
-			if s.get("color_scheme") == app_settings.get("day"):
-				sublime.run_command("change_night")
+		if s.get("color_scheme") != app_settings.get("night") and s.get("color_scheme") == app_settings.get("day"):
+			sublime.run_command("change_night")
 	elif int(hour) >= 06:
-		if s.get("color_scheme") != app_settings.get("day"):
-			if s.get("color_scheme") == app_settings.get("night"):
-				sublime.run_command("change_day")
+		if s.get("color_scheme") != app_settings.get("day") and s.get("color_scheme") == app_settings.get("night"):
+			sublime.run_command("change_day")
+	sublime.set_timeout(doTimeCheck, 60*1000) # 60 seconds
 
-sublime.set_timeout(doTimeCheck, 600000) # 10 minutes
-'''
+doTimeCheck()
+#sublime.set_timeout(doTimeCheck, 600000) # 10 minutes
