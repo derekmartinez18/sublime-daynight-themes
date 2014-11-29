ecdb2ea7e95b5679e5afd34b7bdab18f814caea6
Derek Martinez <me@derek-martinez.us>
2014-11-28 23:31:50 -0500

    Stupid paths

--- NightDay.py
+++ NightDay.py
@@ -15,16 +15,43 @@ class change_dayCommand(sublime_plugin.ApplicationCommand):
 		s.set("color_scheme", app_settings.get("day"))
 		sublime.save_settings("../User/Preferences.sublime-settings")
 
+class change_enabledCommand(sublime_plugin.ApplicationCommand):
+	def run(self):
+		app_settings = sublime.load_settings("NightDay.sublime-settings")
+		if app_settings.get("automatic", "yes") == "yes":
+			app_settings.set("automatic", "no")
+		else:
+			app_settings.set("automatic", "yes")
+
+		sublime.save_settings("NightDay.sublime-settings")
+
+
 def doTimeCheck():
 	hour = time.strftime("%H")
 	app_settings = sublime.load_settings("NightDay.sublime-settings")
 	s = sublime.load_settings("../User/Preferences.sublime-settings")
 	if int(hour) >= 20:
-		if s.get("color_scheme") != app_settings.get("night") and s.get("color_scheme") == app_settings.get("day"):
+		if s.get("color_scheme") != app_settings.get("night") and s.get("color_scheme") == app_settings.get("day") and app_settings.get("automatic") == "yes":
 			sublime.run_command("change_night")
 	elif int(hour) >= 06:
-		if s.get("color_scheme") != app_settings.get("day") and s.get("color_scheme") == app_settings.get("night"):
+		if s.get("color_scheme") != app_settings.get("day") and s.get("color_scheme") == app_settings.get("night") and app_settings.get("automatic") == "yes":
 			sublime.run_command("change_day")
-	sublime.set_timeout(doTimeCheck, 60*1000) # 60 seconds
 
-doTimeCheck()
+	if app_settings.get("automatic") == "yes":
+		sublime.set_timeout(doTimeCheck, 60*1000) # 60 seconds
+
+app_settings = sublime.load_settings("NightDay.sublime-settings")
+if app_settings.has("automatic"):
+	if app_settings.get("automatic") == "yes":
+		doTimeCheck()
+else:
+	app_settings.set("automatic", "yes")
+	sublime.save_settings("NightDay.sublime-settings")
+
+if not app_settings.has("night"):
+	app_settings.set("night", "{insert path to theme here}")
+	sublime.save_settings("NightDay.sublime-settings")
+
+if not app_settings.has("day"):
+	app_settings.set("day", "{insert path to theme here}")
+	sublime.save_settings("NightDay.sublime-settings")
